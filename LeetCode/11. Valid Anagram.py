47512b1ce24b3324890612fbe868054e1dd3a98a
HS001YT <singlaharshit480@gmail.com>
2025-10-04 18:41:55 +0530

    Day 11

--- LeetCode/11. Valid Anagram.py
+++ LeetCode/11. Valid Anagram.py
@@ -3,11 +3,25 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        # We have two approch to solve this problem
+        if len(s) != len(t):
+            return False
+
+        Scount = Tcount ={}
+
+        for i in range(len(s)):
+            Scount[s[i]] = 1 + Scount.get(s[i], 0)
+            Tcount[t[i]] = 1 + Tcount.get(t[i], 0)
+        
+        for j in Scount:
+            if Scount[j] != Tcount.get(j, 0):
+                return False
+        return True
+
+        # We also have two built - in approch to solve this problem
         # 1) Sort both string then return the comparing result
         # return sorted(s) == sorted(t)
         # But this approch has Time - O(n log₁₀ n) and Space - O(n) in python as it creates a new list
 
         # 2) Incresing the counter which is lighter then the sorting
         # counter of 26 alphabets which gives Time - O(n) and Space - O(1)
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)

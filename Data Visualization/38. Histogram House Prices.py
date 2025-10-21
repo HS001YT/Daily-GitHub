754cb49c4fb5e2d699b98e57dc2e38b4d9d9822e
HS001YT <singlaharshit480@gmail.com>
2025-10-21 16:34:09 +0530

    Day 38

--- Data Visualization/38. Histogram House Prices.py
+++ Data Visualization/38. Histogram House Prices.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
+import pandas as pd;
 import seaborn as sns
 
 # Load Titanic dataset
-titanic = sns.load_dataset("titanic")
+data = pd.read_csv(r"D:\Daily-GitHub\Data Visualization\other_files\USA Housing Dataset.csv")
+data = data[data["price"] < 2_000_000]
 
-ages = titanic['age']       
-# Other columns list- [survived, pclass, sex, age, sibsp, parch, fare, embarked, class, who, adult_male, deck, embark_town, alive, alone]
+plt.figure(figsize=(8, 6))
+plt.grid(axis="y", linestyle="--", alpha=0.7)
 
-sns.histplot(data=titanic, x="age", bins=20, kde=True, color="skyblue", element="step") # Remove element to see normal histogram view
-plt.title("Age Distribution Histogram (Titanic Dataset)")
-plt.xlabel("Age")
-plt.ylabel("Numbers")
+sns.histplot(data=data, x="price", bins=20, kde=True, color="skyblue") # Remove element to see normal histogram view
+plt.title("House Prices", fontsize=16, fontweight="bold")
+plt.xlabel("Prices", fontweight="bold")
+plt.ylabel("Numbers", fontweight="bold")
+plt.tight_layout()
 
-plt.annotate("Source: Seaborn", 
-             xy=(0.7, 0.8), xycoords="figure fraction",
+plt.annotate("Source: Kaggle", 
+             xy=(0.01, 0.03), xycoords="figure fraction",
              ha="left", fontsize=9, color="gray")
 
-plt.get_current_fig_manager().set_window_title("Day 14")
-# plt.savefig("D:\Daily-GitHub\Data Visualization\other_files\Day14.png", dpi=300, bbox_inches="tight")
+plt.get_current_fig_manager().set_window_title("Day 38")
+# plt.savefig("D:\Daily-GitHub\Data Visualization\other_files\Day38.png", dpi=300, bbox_inches="tight")
 
 plt.show()

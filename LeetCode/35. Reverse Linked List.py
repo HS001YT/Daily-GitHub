52b217f38c1d564f3c2f5b56361d2743467b299a
HS001YT <singlaharshit480@gmail.com>
2025-10-21 15:38:02 +0530

    Day 35

---
+++ LeetCode/35. Reverse Linked List.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         prev = None
+#         curr = head
+        
+#         while curr:
+#             next_temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next_temp
+        
+#         return prev
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return new_head

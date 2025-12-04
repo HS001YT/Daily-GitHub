cbec0e8e9bc4c801bd63fd38c5b8afaf3bfff5cc
HS001YT <singlaharshit480@gmail.com>
2025-12-04 16:02:25 +0530

    Day 61

---
+++ Problem Solving/LeetCode/61. Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,31 @@
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return nullptr;
+
+        ListNode* curr = head;
+
+        while (curr->next) {
+            if (curr->val == curr->next->val) {
+                // Duplicate found, remove the next node
+                ListNode* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            } else {
+                curr = curr->next;
+            }
+        }
+
+        return head;
+    }
+};

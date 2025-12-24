d84680449342ece76a8f21e7359f2b5a7eaa3768
HS001YT <singlaharshit480@gmail.com>
2025-12-24 18:49:34 +0530

    Day 107

---
+++ Problem Solving/LeetCode/107. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;           // 1 step
+            fast = fast->next->next;     // 2 steps
+
+            if (slow == fast)
+                return true;             // cycle found
+        }
+        return false;                    // no cycle
+    }
+};

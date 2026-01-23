01dcd3099c04c92afb550f85b766f92a407a8cba
HS001YT <singlaharshit480@gmail.com>
2026-01-23 18:03:03 +0530

    Day 137

---
+++ Problem Solving/LeetCode/137. Validate Binary Search Tree.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, LLONG_MIN, LLONG_MAX);
+    }
+
+private:
+    bool isValid(TreeNode* node, long long minVal, long long maxVal) {
+        if (!node) return true;
+
+        if (node->val <= minVal || node->val >= maxVal)
+            return false;
+
+        return isValid(node->left, minVal, node->val) &&
+               isValid(node->right, node->val, maxVal);
+    }
+};

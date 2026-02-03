b4e0eb470b0211ae62581031e2045f6bd8ed678e
HS001YT <singlaharshit480@gmail.com>
2026-02-03 20:06:10 +0530

    Day 146

---
+++ Problem Solving/LeetCode/146. Course Schedule.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        // Build graph and indegree
+        for (auto &p : prerequisites) {
+            graph[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+
+        queue<int> q;
+        // Push courses with no prerequisites
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int completed = 0;
+
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            completed++;
+
+            for (int next : graph[course]) {
+                indegree[next]--;
+                if (indegree[next] == 0)
+                    q.push(next);
+            }
+        }
+
+        return completed == numCourses;
+    }
+};
